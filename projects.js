22ee051907415ef9e2652407cc725a4dbc6f8a0a
surafel-47 <surafelzewdu2018@gmail.com>
2025-02-27 00:36:22 +0300

    Update project entries with new descriptions, links, and media for enhanced portfolio presentation

--- projects.js
+++ projects.js
@@ -1,24 +1,61 @@
 const projects = {
     website: [
       {
-        title: "Project 1",
-        description: "This is a description for Project 1.",
-        githubLink: "https://github.com/project1",
-        liveLink: "https://project1.com",
+        title: "Ethiopian Road Authority Web-portal",
+        description: "A Python Django Web-portal for Ethiopian Road Authority. Axle Load/Veichle Load Managment and Monitoring System",
+        liveLink: "https://irams.era.gov.et/",
         media: [
-          "https://example.com/image1.jpg",
-          "https://example.com/image2.jpg"
+          "/assets/projects/EraRasm/2.png",
+          "/assets/projects/EraRasm/3.png",
+          "/assets/projects/EraRasm/1.png",
         ]
       },
       {
-        title: "Project 2",
-        description: "This is a description for Project 2.",
-        githubLink: "https://github.com/project2",
-        liveLink: "https://project2.com",
+        title: "Et Legal Chatbot",
+        description: "A RAG based Chatbot for Answering Ethiopian Legal Questions, it uses AI to answer legal questions and provide legal advice to users. It uses Python and Node.js for the backend and Vue.js frontend",
+        githubLink: "https://github.com/Xiemingminsan/legal-ai",
+        liveLink: "https://etlegalchat.tech/",
         media: [
-          "https://example.com/image3.jpg"
+          "/assets/projects/etlegalchat/1.png",
+          "/assets/projects/etlegalchat/2.png",
+          "/assets/projects/etlegalchat/3.png",
         ]
-      }
+      },
+      {
+        title: "Tesde Social Media Platform",
+        description: "A blog Sharing Site made with Vue.js, Tailwind and backend with Node.js(Express) and MongoDB. Users can create, read, update, and delete their posts and stories. It has a user authentication system and uses JWT for token-based authentication.",
+        liveLink: "https://github.com/KaleabHegie/Social-Media-Platform",
+        media: [
+          // "/assets/projects/Tesde/1.png",
+          // "/assets/projects/Tesde/2.png",
+          // "/assets/projects/Tesde/3.png",
+        ]
+      },
+      {
+        title: "Addis Times Blog Sharing Platform",
+        description: "A blog Sharing Site made with Vue.js, Tailwind and backend with Node.js(Express) and MongoDB. Users can create, read, update, and delete their blogs. It has a user authentication system and uses JWT for token-based authentication.",
+        githubLink: "https://github.com/surafel-47/blog_mmcy",
+        // liveLink: "https://project2.com",
+        media: [
+          "/assets/projects/AddisTimes/1.jpg",
+          "/assets/projects/AddisTimes/2.jpg",
+          "/assets/projects/AddisTimes/3.jpg",
+
+        ]
+      },
+      {
+        title: "Mercury Shopping Web app and API for Mobile Platform",
+        description: "A Model-View-Controller (MVC) architecture-style platform for shopping. Customers can sign up, sign in, and use the website or the mobile application. It uses Entity Framework to integrate SQL Server Database and Object-Table mapping. It also has an API Controller that responds to the mobile applications request!",
+        githubLink: "https://github.com/surafel-47/MercuryShopping_ASP.NET",
+        media: [
+          "/assets/projects/ShoppingAsp/1.png",
+          "/assets/projects/ShoppingAsp/2.png",
+          "/assets/projects/ShoppingAsp/3.png",
+          "/assets/projects/ShoppingAsp/4.png",
+          "/assets/projects/ShoppingAsp/5.png",
+          "/assets/projects/ShoppingAsp/6.png",
+        ]
+      },
       // Add more website projects as needed
     ],
     mobile: [
@@ -32,6 +69,61 @@ const projects = {
           "/assets/projects/crooze/2.jpg",
           "/assets/projects/crooze/3.jpg",
           "/assets/projects/crooze/4.jpg",
+        ]
+      }
+      ,      
+      {
+        title: "School Facial Recogintion Based Attendance App",
+        description: "An AI Platform for student attendance management where both students and teachers can login in moniter there class attendance and stats, it uses AI to scan the students present in the class room base on classroom photo and facial recongintion techonology!",
+        githubLink: "https://github.com/surafel-47/hilcoe_attendance_app",
+        media: [
+          "/assets/projects/AttendanceApp/1.jpg",
+          "/assets/projects/AttendanceApp/2.jpg",
+          "/assets/projects/AttendanceApp/3.jpg",
+          "/assets/projects/AttendanceApp/4.jpg",
+        ]
+      }
+      ,
+      {
+        title: "Wallpaper App",
+        description: "Mobile App made with flutter using pixabay API's to Fetch Wallpaper from there backend. Users can see trending Wallpaper, Recent Wallpaper and Search Wallpapers. Users can save, and Download there favorite images. Has DarkMode feature. Uses flutter state provider.",
+        githubLink: "https://github.com/surafel-47/wally_wallpaperapp",
+        media: [
+          "/assets/projects/WallpaperApp/1.jpg",
+          "/assets/projects/WallpaperApp/2.jpg",
+          "/assets/projects/WallpaperApp/3.jpg",
+          "/assets/projects/WallpaperApp/4.jpg",
+        ]
+      },
+      {
+        title: "Wef Mobile Weather App",
+        description: "A mobile App i made with flutter that uses the Open Weather API to fetch and show the current weather around different cities across the world. This app uses flutter state provider making it seemlessly fast!",
+        githubLink: "https://github.com/surafel-47/wefweatherapp",
+        media: [
+          "/assets/projects/WeatherApp/1.jpg",
+          "/assets/projects/WeatherApp/2.jpg",
+          "/assets/projects/WeatherApp/3.jpg",
+          "/assets/projects/WeatherApp/4.jpg",
+        ]
+      },
+      {
+        title: "Mercury Shopping App",
+        description: "An Ecommerce Application for customers shopping, it has backend ASP.NET server made and Intergrated.",
+        githubLink: "https://github.com/surafel-47/mercury_shopping_app",
+        media: [
+          "/assets/projects/Shopping/1.jpg",
+          "/assets/projects/Shopping/2.jpg",
+          "/assets/projects/Shopping/3.jpg",
+          "/assets/projects/Shopping/4.jpg",
+        ]
+      },
+      {
+        title: "Calculator App",
+        description: "A fast and simple calulator for computing maths expressions. Has DarkMode and uses State Proivider Package!",
+        githubLink: "https://github.com/surafel-47/calculator_app",
+        media: [
+          "/assets/projects/Calculator/1.jpg",
+          "/assets/projects/Calculator/2.jpg",
         ]
       }
       // Add more mobile projects as needed
@@ -50,12 +142,16 @@ const projects = {
     ],
     csharp: [
       {
-        title: "C# Project 1",
-        description: "This is a description for C# Project 1.",
-        githubLink: "https://github.com/csharp1",
-        liveLink: "https://csharp1.com",
+        title: "Zues Supermarket/shop Inventory Managment Program",
+        description: "A WinForm application with SQL server database that is related A shops Inventory Managment. Both admin and from desk chashier can log into the program and access different capabilities like Invertory modification and creating/logging transactions",
+        githubLink: "https://github.com/surafel-47/Zues_Supermarket_mgt",
         media: [
-          "https://example.com/csharp_image1.jpg"
+          "/assets/projects/CSharp/1.png",
+          "/assets/projects/CSharp/2.png",
+          "/assets/projects/CSharp/3.png",
+          "/assets/projects/CSharp/4.png",
+          "/assets/projects/CSharp/5.png",
+
         ]
       }
       // Add more C# projects as needed
