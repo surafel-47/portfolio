98facd4f270b585b5ff8f13dc117e80418496696
surafel-47 <surafelzewdu2018@gmail.com>
2025-03-21 13:54:16 +0300

    Enhance navigation and project presentation with updated links and new Fancybox integration

--- projects.js
+++ projects.js
@@ -5,9 +5,9 @@ const projects = {
         description: "A Python Django Web-portal for Ethiopian Road Authority. Axle Load/Veichle Load Managment and Monitoring System",
         liveLink: "https://irams.era.gov.et/",
         media: [
-          "/assets/projects/EraRasm/2.png",
-          "/assets/projects/EraRasm/3.png",
-          "/assets/projects/EraRasm/1.png",
+          "assets/projects/EraRasm/2.png",
+          "assets/projects/EraRasm/3.png",
+          "assets/projects/EraRasm/1.png",
         ]
       },
       {
@@ -16,9 +16,9 @@ const projects = {
         githubLink: "https://github.com/Xiemingminsan/legal-ai",
         liveLink: "https://etlegalchat.tech/",
         media: [
-          "/assets/projects/etlegalchat/1.png",
-          "/assets/projects/etlegalchat/2.png",
-          "/assets/projects/etlegalchat/3.png",
+          "assets/projects/etlegalchat/1.png",
+          "assets/projects/etlegalchat/2.png",
+          "assets/projects/etlegalchat/3.png",
         ]
       },
       {
@@ -26,9 +26,9 @@ const projects = {
         description: "A blog Sharing Site made with Vue.js, Tailwind and backend with Node.js(Express) and MongoDB. Users can create, read, update, and delete their posts and stories. It has a user authentication system and uses JWT for token-based authentication.",
         liveLink: "https://github.com/KaleabHegie/Social-Media-Platform",
         media: [
-          // "/assets/projects/Tesde/1.png",
-          // "/assets/projects/Tesde/2.png",
-          // "/assets/projects/Tesde/3.png",
+          // "assets/projects/Tesde/1.png",
+          // "assets/projects/Tesde/2.png",
+          // "assets/projects/Tesde/3.png",
         ]
       },
       {
@@ -37,9 +37,9 @@ const projects = {
         githubLink: "https://github.com/surafel-47/blog_mmcy",
         // liveLink: "https://project2.com",
         media: [
-          "/assets/projects/AddisTimes/1.jpg",
-          "/assets/projects/AddisTimes/2.jpg",
-          "/assets/projects/AddisTimes/3.jpg",
+          "assets/projects/AddisTimes/1.jpg",
+          "assets/projects/AddisTimes/2.jpg",
+          "assets/projects/AddisTimes/3.jpg",
 
         ]
       },
@@ -48,12 +48,12 @@ const projects = {
         description: "A Model-View-Controller (MVC) architecture-style platform for shopping. Customers can sign up, sign in, and use the website or the mobile application. It uses Entity Framework to integrate SQL Server Database and Object-Table mapping. It also has an API Controller that responds to the mobile applications request!",
         githubLink: "https://github.com/surafel-47/MercuryShopping_ASP.NET",
         media: [
-          "/assets/projects/ShoppingAsp/1.png",
-          "/assets/projects/ShoppingAsp/2.png",
-          "/assets/projects/ShoppingAsp/3.png",
-          "/assets/projects/ShoppingAsp/4.png",
-          "/assets/projects/ShoppingAsp/5.png",
-          "/assets/projects/ShoppingAsp/6.png",
+          "assets/projects/ShoppingAsp/1.png",
+          "assets/projects/ShoppingAsp/2.png",
+          "assets/projects/ShoppingAsp/3.png",
+          "assets/projects/ShoppingAsp/4.png",
+          "assets/projects/ShoppingAsp/5.png",
+          "assets/projects/ShoppingAsp/6.png",
         ]
       },
       // Add more website projects as needed
@@ -65,10 +65,10 @@ const projects = {
         // githubLink: "https://github.com/mobile1",
         // liveLink: "https://crooze.com",
         media: [
-          "/assets/projects/crooze/1.jpg",
-          "/assets/projects/crooze/2.jpg",
-          "/assets/projects/crooze/3.jpg",
-          "/assets/projects/crooze/4.jpg",
+          "assets/projects/crooze/1.jpg",
+          "assets/projects/crooze/2.jpg",
+          "assets/projects/crooze/3.jpg",
+          "assets/projects/crooze/4.jpg",
         ]
       }
       ,      
@@ -77,10 +77,10 @@ const projects = {
         description: "An AI Platform for student attendance management where both students and teachers can login in moniter there class attendance and stats, it uses AI to scan the students present in the class room base on classroom photo and facial recongintion techonology!",
         githubLink: "https://github.com/surafel-47/hilcoe_attendance_app",
         media: [
-          "/assets/projects/AttendanceApp/1.jpg",
-          "/assets/projects/AttendanceApp/2.jpg",
-          "/assets/projects/AttendanceApp/3.jpg",
-          "/assets/projects/AttendanceApp/4.jpg",
+          "assets/projects/AttendanceApp/1.jpg",
+          "assets/projects/AttendanceApp/2.jpg",
+          "assets/projects/AttendanceApp/3.jpg",
+          "assets/projects/AttendanceApp/4.jpg",
         ]
       }
       ,
@@ -89,10 +89,10 @@ const projects = {
         description: "Mobile App made with flutter using pixabay API's to Fetch Wallpaper from there backend. Users can see trending Wallpaper, Recent Wallpaper and Search Wallpapers. Users can save, and Download there favorite images. Has DarkMode feature. Uses flutter state provider.",
         githubLink: "https://github.com/surafel-47/wally_wallpaperapp",
         media: [
-          "/assets/projects/WallpaperApp/1.jpg",
-          "/assets/projects/WallpaperApp/2.jpg",
-          "/assets/projects/WallpaperApp/3.jpg",
-          "/assets/projects/WallpaperApp/4.jpg",
+          "assets/projects/WallpaperApp/1.jpg",
+          "assets/projects/WallpaperApp/2.jpg",
+          "assets/projects/WallpaperApp/3.jpg",
+          "assets/projects/WallpaperApp/4.jpg",
         ]
       },
       {
@@ -100,10 +100,10 @@ const projects = {
         description: "A mobile App i made with flutter that uses the Open Weather API to fetch and show the current weather around different cities across the world. This app uses flutter state provider making it seemlessly fast!",
         githubLink: "https://github.com/surafel-47/wefweatherapp",
         media: [
-          "/assets/projects/WeatherApp/1.jpg",
-          "/assets/projects/WeatherApp/2.jpg",
-          "/assets/projects/WeatherApp/3.jpg",
-          "/assets/projects/WeatherApp/4.jpg",
+          "assets/projects/WeatherApp/1.jpg",
+          "assets/projects/WeatherApp/2.jpg",
+          "assets/projects/WeatherApp/3.jpg",
+          "assets/projects/WeatherApp/4.jpg",
         ]
       },
       {
@@ -111,10 +111,10 @@ const projects = {
         description: "An Ecommerce Application for customers shopping, it has backend ASP.NET server made and Intergrated.",
         githubLink: "https://github.com/surafel-47/mercury_shopping_app",
         media: [
-          "/assets/projects/Shopping/1.jpg",
-          "/assets/projects/Shopping/2.jpg",
-          "/assets/projects/Shopping/3.jpg",
-          "/assets/projects/Shopping/4.jpg",
+          "assets/projects/Shopping/1.jpg",
+          "assets/projects/Shopping/2.jpg",
+          "assets/projects/Shopping/3.jpg",
+          "assets/projects/Shopping/4.jpg",
         ]
       },
       {
@@ -122,8 +122,8 @@ const projects = {
         description: "A fast and simple calulator for computing maths expressions. Has DarkMode and uses State Proivider Package!",
         githubLink: "https://github.com/surafel-47/calculator_app",
         media: [
-          "/assets/projects/Calculator/1.jpg",
-          "/assets/projects/Calculator/2.jpg",
+          "assets/projects/Calculator/1.jpg",
+          "assets/projects/Calculator/2.jpg",
         ]
       }
       // Add more mobile projects as needed
@@ -146,11 +146,11 @@ const projects = {
         description: "A WinForm application with SQL server database that is related A shops Inventory Managment. Both admin and from desk chashier can log into the program and access different capabilities like Invertory modification and creating/logging transactions",
         githubLink: "https://github.com/surafel-47/Zues_Supermarket_mgt",
         media: [
-          "/assets/projects/CSharp/1.png",
-          "/assets/projects/CSharp/2.png",
-          "/assets/projects/CSharp/3.png",
-          "/assets/projects/CSharp/4.png",
-          "/assets/projects/CSharp/5.png",
+          "assets/projects/CSharp/1.png",
+          "assets/projects/CSharp/2.png",
+          "assets/projects/CSharp/3.png",
+          "assets/projects/CSharp/4.png",
+          "assets/projects/CSharp/5.png",
 
         ]
       }
